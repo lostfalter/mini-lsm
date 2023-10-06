afeb46a56ae550d1ec5add3857b09b4c2a2c8883
lostfly <lostfalter@gmail.com>
2023-10-06 14:41:22 +0800

    Complete block and block_builder implementation

--- mini-lsm-starter/src/block.rs
+++ mini-lsm-starter/src/block.rs
@@ -1,12 +1,12 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
+// #![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
+// #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
 mod builder;
 mod iterator;
 
 pub use builder::BlockBuilder;
 /// You may want to check `bytes::BufMut` out when manipulating continuous chunks of memory
-use bytes::Bytes;
+use bytes::{BufMut, Bytes, BytesMut};
 pub use iterator::BlockIterator;
 
 /// A block is the smallest unit of read and caching in LSM tree.
@@ -27,12 +27,36 @@ impl Block {
     /// Encode the internal data to the data layout illustrated in the tutorial
     /// Note: You may want to recheck if any of the expected field is missing from your output
     pub fn encode(&self) -> Bytes {
-        unimplemented!()
+        let mut buf = BytesMut::with_capacity(self.data.len() + (self.offsets.len() * 2) + 2);
+        buf.put(self.data.as_slice());
+        for offset in self.offsets.iter() {
+            buf.put_u16_le(*offset);
+        }
+        buf.put_u16_le(self.offsets.len() as u16);
+
+        buf.into()
     }
 
     /// Decode from the data layout, transform the input `data` to a single `Block`
     pub fn decode(data: &[u8]) -> Self {
-        unimplemented!()
+        let element_count = u16::from_le_bytes([data[data.len() - 2], data[data.len() - 1]]);
+
+        let entry_bytes_count = data.len() - (2 * element_count as usize) - 2;
+
+        let mut block = Block {
+            data: Vec::with_capacity(entry_bytes_count),
+            offsets: Vec::with_capacity(element_count as usize * 2),
+        };
+
+        block.data.put(&data[0..entry_bytes_count]);
+        for i in 0..element_count as usize {
+            block.offsets.push(u16::from_le_bytes([
+                data[entry_bytes_count + (i * 2)],
+                data[entry_bytes_count + (i * 2) + 1],
+            ]));
+        }
+
+        block
     }
 }
 
--- mini-lsm-starter/src/block/builder.rs
+++ mini-lsm-starter/src/block/builder.rs
@@ -1,62 +1,78 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
+// #![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
+// #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
+
+use bytes::BufMut;
 
 use super::Block;
 
 /// Builds a block.
 pub struct BlockBuilder {
     buffer: Vec<u8>,
-    forward_pos: usize,
-    backward_pos: usize,
+    element_count: usize,
 }
 
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
         BlockBuilder {
-            buffer: vec![0; block_size],
-            forward_pos: 0,
-            backward_pos: 0,
+            buffer: Vec::with_capacity(block_size),
+            element_count: 0,
         }
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: &[u8], value: &[u8]) -> bool {
-        let remained_bytes = self.backward_pos - self.forward_pos - 3;
         let entry_bytes = key.len() + value.len() + 4;
-        if key.len() + value.len() + 4 > remained_bytes {
-            return false;
+        if !self.is_empty() && entry_bytes + self.current_ocuppied_bytes() > self.buffer.capacity()
+        {
+            false
         } else {
-            self.append_entry_bytes(&(key.len() as u16).to_le_bytes());
-            self.append_entry_bytes(key);
-            self.append_entry_bytes(&(value.len() as u16).to_le_bytes());
-            self.append_entry_bytes(value);
+            self.buffer.put((key.len() as u16).to_le_bytes().as_slice());
+            self.buffer.put(key);
+            self.buffer
+                .put((value.len() as u16).to_le_bytes().as_slice());
+            self.buffer.put(value);
 
-            self.add_offset(entry_bytes as u16);
+            self.element_count = self.element_count + 1;
 
-            return true;
+            true
         }
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        return self.element_count == 0;
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        let mut block = Block {
+            data: self.buffer,
+            offsets: Vec::with_capacity(self.element_count),
+        };
+
+        Self::build_offsets(&mut block.offsets, &block.data);
+
+        return block;
     }
 
-    fn last_element_offset(&self) -> usize {
-        return 0;
+    fn current_ocuppied_bytes(&self) -> usize {
+        return self.buffer.len() + (self.element_count + 1) * 2;
     }
 
-    fn append_entry_bytes(&mut self, bytes: &[u8]) {
-        self.buffer[self.forward_pos..self.forward_pos + bytes.len()].copy_from_slice(bytes);
-        self.forward_pos += bytes.len();
-    }
+    fn build_offsets(offsets: &mut Vec<u16>, buffer: &Vec<u8>) {
+        // scan the offset
+        let mut last_offset = 0;
+        let mut buf_pos = 0;
+        while buf_pos < buffer.len() {
+            let key_len = u16::from_le_bytes([buffer[buf_pos], buffer[buf_pos + 1]]);
+            buf_pos += key_len as usize + 2;
+            let value_len = u16::from_le_bytes([buffer[buf_pos], buffer[buf_pos + 1]]);
+            buf_pos += value_len as usize + 2;
 
-    fn add_offset(&mut self, entry_bytes: u16) {}
+            offsets.push(last_offset);
+            last_offset = last_offset + key_len + value_len + 4;
+        }
+    }
 }
